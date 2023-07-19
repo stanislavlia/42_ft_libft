7e05515dc19514ad5964394e496b878314c8dbf6
stanislavlia <stanislavliashkov@gmail.com>
2023-07-19 16:49:37 +0700

    Ft_memset done and checked. It should work correctly

--- srcs/ft_memset.c
+++ srcs/ft_memset.c
@@ -1,43 +1,16 @@
-
-
-
-
 void    *ft_memset(void *ptr, int val, unsigned long num)
 {
     unsigned char   val_to_set;
-    int i;
-    unsigned char *char_p;
+    int             i;
+    unsigned char   *char_p;
 
     char_p = (char *)ptr;
-    val_to_set = val % 256;
     i = 0;
     while (i < num)
     {
-        *char_p = val_to_set;
+        *char_p = (unsigned char)val_to_set;
         char_p++;
         i++;
     }
-    
     return (ptr);
 }
-
-/*
-#include <stdio.h>
-#include <string.h>
-
-int main() {
-    char    str[10];
-    int     i;
-
-    //ft_memset(str, 77, sizeof(char) * 3 );  // Initialize all elements of arr[] to 0
-    memset(str, 77, sizeof(char) * 3 );
-
-    // Print the array
-    for (i=0; i < 10; i++)
-    {
-        printf("%d \n", str[i]);
-    }
-
-    return 0;
-}
-*/
