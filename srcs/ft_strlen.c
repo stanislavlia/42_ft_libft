c2f68319cedb9fef2fd487ce912b3a33213f3a1e
stanislavlia <stanislavliashkov@gmail.com>
2023-07-22 19:04:15 +0700

    removed main from ft_strlen.c

--- srcs/ft_strlen.c
+++ srcs/ft_strlen.c
@@ -10,34 +10,3 @@ unsigned int	ft_strlen(char *string)
 	}
 	return (len);
 }
-
-#include <string.h>
-#include <stdio.h>
-
-int main(void)
-{
-    const char *test_strings[] = {
-        "Hello, world!",
-        "1234567890",
-        "A string with spaces",
-        "",
-        "A very, very, very, very, very, very, very, very, very, very long string",
-        NULL
-    };
-
-    for (int i = 0; test_strings[i] != NULL; ++i)
-    {
-        const char *s = test_strings[i];
-        if (strlen(s) == ft_strlen(s))
-        {
-            printf("PASS: \"%s\"\n", s);
-        }
-        else
-        {
-            printf("FAIL: \"%s\"\n", s);
-        }
-    }
-
-    return 0;
-}
-
